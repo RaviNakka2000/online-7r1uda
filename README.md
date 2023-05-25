c7b3cb84fdbf7a093d56c76d8d4012ccbd7308ff
RaviNakka2000 <134619763+RaviNakka2000@users.noreply.github.com>
2023-05-25 19:05:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# online-7r1uda
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/online-7r1uda)
---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent,
+      },
+    ]),
+  ],
+  declarations: [AppComponent, HomeComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
